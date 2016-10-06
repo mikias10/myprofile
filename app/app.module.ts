3395e52d6542ea33d34d468a13a31f4821418f39
mike <owner@MyMacBookPro.local>
2016-10-06 07:20:25 -0400

    Create home and about components

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component'
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component'
+import { AboutComponent } from './components/pages/about.component'
+import { HomeComponent } from './components/pages/home.component'
+
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, NavbarComponent, JumbotronComponent],
+  declarations: [
+          AppComponent,
+          NavbarComponent,
+          JumbotronComponent,
+          AboutComponent,
+          HomeComponent
+      ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
