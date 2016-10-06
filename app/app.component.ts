111455f897f97182d7d34676ea4ce83c42716d0a
mike <owner@MyMacBookPro.local>
2016-10-06 06:30:07 -0400

    Modify navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-
     selector: 'my-app',
-    template: `<h1>My First Angular App</h1>`
-
+    template: `<h1>My First Angular App</h1>
+        <navbar></navbar>
+      `
 })
 
 export class AppComponent { }
---
+++ app/components/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar' ,
+  template: 'NAVBAR'
+})
+
+export class NavbarComponent { }
